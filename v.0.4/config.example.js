4b7a0e5bd6565fc4e7fb7e6ddea5364206814e29
Igor Feoktistov <feoktistov.igor@gmail.com>
2015-01-26 15:28:18 +0200

    copy 0.3.2 to 0.4 folder for check diffs in future

---
+++ v.0.4/config.example.js
@@ -0,0 +1,13 @@
+module.exports = {
+    managePort : 4440,
+    fetchPort : 88,
+    amqp : {
+        host: "127.0.0.1",
+        login: 'guest',
+        password: 'guest',
+        queue: 'messages'
+    },
+    'longPolling' : {
+        timeout : 60000 //ms
+    }
+}
